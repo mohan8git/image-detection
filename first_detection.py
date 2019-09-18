3e17a85a411f7a437a1147547d42815dc2d7b7f0
Mohan Bishnoi <47236609+mohan8git@users.noreply.github.com>
2019-09-18 21:51:47 +0530

    Update first_detection.py

--- first_detection.py
+++ first_detection.py
@@ -8,6 +8,6 @@ detector.setModelTypeAsRetinaNet()
 detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
 detector.loadModel()
 detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "flower.jpg"), output_image_path=os.path.join(execution_path , "flower1.jpg"))
-
+#here in above line replace the flower.jpg with image file you want to detect and then in place of flower1.jpg write the destination file name
 for eachObject in detections:
     print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
